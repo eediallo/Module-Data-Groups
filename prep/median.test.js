6feeba124536a03ba12608610b1263ccf7e2d3bb
eediallo <elhhabdouldiallo@gmail.com>
2024-10-27 21:45:41 +0000

    import calculateMedian function

--- prep/median.test.js
+++ prep/median.test.js
@@ -1,3 +1,6 @@
+const medianLib = require("./median");
+const calculateMedian = medianLib.calculateMedian;
+
 test("calculates the median of a list of odd length", () => {
   const list = [10, 20, 30, 50, 60];
   const currentOutPut = calculateMedian(list);
