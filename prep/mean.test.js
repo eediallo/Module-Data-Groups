f5eae2aa50d388cfc9b0beaa04d17ed5317b6c16
eediallo <elhhabdouldiallo@gmail.com>
2024-10-27 21:26:48 +0000

    creates calculateMean function and make it first case passed

--- prep/mean.test.js
+++ prep/mean.test.js
@@ -1,9 +1,14 @@
+const sumValuesLib = require("./sumValues");
+const sumValues = sumValuesLib.sumValues;
+
+
+function calculateMean(list) {
+  return sumValues(list) / list.length;
+}
+
 test("Calculates the mean of a list of numbers", () => {
   const list = [3, 50, 7];
-
   const currentOutput = calculateMean(list);
-
   const targetOutput = 20;
-
   expect(currentOutput).toBe(targetOutput);
 });
