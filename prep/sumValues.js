81ac138853a9d67a690929a83686b5d54d173090
eediallo <elhhabdouldiallo@gmail.com>
2024-10-27 21:28:08 +0000

    removes unused code

--- prep/sumValues.js
+++ prep/sumValues.js
@@ -1,13 +1,3 @@
-// function sumValues(list) {
-//   let total = 0;
-
-//   for (let i = 0; i < list.length; i++) {
-//     total += list[i];
-//   }
-
-//   return total;
-// }
-
 //sumValues sum the items of the list array and return the sum
 const sumValues = (list) => list.reduce((acc, curr) => acc + curr, 0);
 
