09573d5d4c1e1652ffec640e0e659172d7061556
eediallo <elhhabdouldiallo@gmail.com>
2024-10-27 20:49:24 +0000

    creates a function to sum the values of list array

--- prep/mean.test.js
+++ prep/mean.test.js
@@ -1,9 +1,24 @@
-test("Calculates the mean of a list of numbers", () => {
-  const list = [3, 50, 7];
+// test("Calculates the mean of a list of numbers", () => {
+//   const list = [3, 50, 7];
 
-  const currentOutput = calculateMean(list);
+//   const currentOutput = calculateMean(list);
 
-  const targetOutput = 20;
+//   const targetOutput = 20;
 
-  expect(currentOutput).toBe(targetOutput);
-});
+//   expect(currentOutput).toBe(targetOutput);
+// });
+
+
+function sumValues(list) {
+  let total = 0;
+
+  for (let i = 0; i < list.length; i++) {
+    total += list[i];
+  }
+
+  return total;
+}
+
+
+
+console.log(sumValues([2, 5, 5, 3]))
