95587cfef87f6822c080a1800045031ec5953b9b
eediallo <elhhabdouldiallo@gmail.com>
2024-10-27 20:50:22 +0000

    refactor sumValues function

--- prep/mean.test.js
+++ prep/mean.test.js
@@ -9,16 +9,17 @@
 // });
 
 
-function sumValues(list) {
-  let total = 0;
+// function sumValues(list) {
+//   let total = 0;
 
-  for (let i = 0; i < list.length; i++) {
-    total += list[i];
-  }
+//   for (let i = 0; i < list.length; i++) {
+//     total += list[i];
+//   }
 
-  return total;
-}
+//   return total;
+// }
 
-
+//sumValues sum the items of the list array and return the sum
+const sumValues = (list) => list.reduce((acc, curr) => acc + curr, 0)
 
 console.log(sumValues([2, 5, 5, 3]))
