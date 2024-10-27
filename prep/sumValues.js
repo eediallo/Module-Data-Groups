6c6a2e8906ac1a14cb14b2521d1e4430538f0a83
eediallo <elhhabdouldiallo@gmail.com>
2024-10-27 21:36:45 +0000

    changes sumValues.js to mean.js

--- prep/sumValues.js
+++
@@ -1,8 +0,0 @@
-//sumValues sum the items of the list array and return the sum
-const sumValues = (list) => list.reduce((acc, curr) => acc + curr, 0);
-
-module.exports = {
-	sumValues
-};
-
-console.log(sumValues([2, 5, 5, 3]));
