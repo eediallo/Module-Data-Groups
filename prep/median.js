f2ce093a6d13852cd923daa900e5d356167329d7
eediallo <elhhabdouldiallo@gmail.com>
2024-10-28 10:40:05 +0000

    handles cases for even length

--- prep/median.js
+++ prep/median.js
@@ -1,9 +1,18 @@
-// calculateMedian calculates the median of a list of odd length
+// calculateMedian calculates the median of a list of numbers
 function calculateMedian(list) {
-//   const middleIndex = Math.floor(list.length / 2);
-//   return list[middleIndex];
-
-return list
+  const middleIndex = Math.floor(list.length / 2);
+  // sort the list array in an ascending order
+  const sortedArr = list.sort((a, b) => a - b);
+  let median = 0;
+  
+  if (list.length % 2 === 0) {
+    // median for even length elements
+    median += (sortedArr[middleIndex - 1] + sortedArr[middleIndex]) / 2;
+  } else {
+    // median for odd length elements
+    median += sortedArr[middleIndex];
+  }
+  return median;
 }
 
 module.exports = {
