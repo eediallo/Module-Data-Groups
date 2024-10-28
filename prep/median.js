57b03c74183e178c0becbb8557720fe27292e5b4
eediallo <elhhabdouldiallo@gmail.com>
2024-10-28 11:19:48 +0000

    adds function to check the validity of param

--- prep/median.js
+++ prep/median.js
@@ -4,7 +4,22 @@
  * @param {number[]} list - An array of numbers.
  * @returns {number} The median of the list.
  */
+
+function isListAnArray(list) {
+  if (!Array.isArray(list)) {
+    throw new Error(`${list} must be an array`);
+  }
+
+  for (let num of list) {
+    if (!typeof num !== "number") {
+      throw new Error(`${num} must be a number`);
+    }
+  }
+}
+
 function calculateMedian(list) {
+  isListAnArray(list); // check if list is an array
+
   const middleIndex = Math.floor(list.length / 2);
   // sort the list array in an ascending order
   const sortedList = list.sort((a, b) => a - b);
