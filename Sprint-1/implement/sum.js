4042dc145b7be9a0393b397a6aaadee64d2121da
eediallo <elhhabdouldiallo@gmail.com>
2024-10-29 23:00:20 +0000

    imports isEmpty helper function

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,9 +1,10 @@
+const isEmpty = require("./isEmpty.js");
+
 function sum(elements) {
   let sum = 0;
-  if (Array.isArray(elements) && elements.length === 0) {
+  if (isEmpty(elements)) {
     return 0;
   }
-  
   for (let num of elements) {
     if (!isNaN(num) && num % 2 !== 0) {
       sum += parseFloat(num.toFixed(2));
