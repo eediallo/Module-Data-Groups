fe1755fae368205b2224090700bec20fec2b4bc4
eediallo <elhhabdouldiallo@gmail.com>
2024-10-29 22:12:51 +0000

    implement --> returns single element of array with one element

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -2,6 +2,10 @@ function sum(elements) {
   if (Array.isArray(elements) && elements.length === 0) {
     return 0;
   }
+
+  if (elements.length === 1) {
+    return elements[0];
+  }
 }
 
 module.exports = sum;
