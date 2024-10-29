c336251693c0f318e297a91d8a3e45c9f8c0b2ba
eediallo <elhhabdouldiallo@gmail.com>
2024-10-29 19:48:51 +0000

    implement --> deduplicates values in array

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,6 +1,12 @@
 function dedupe(arr) {
-  const copy = arr;
-  return copy;
+  let unique = [];
+  for (let item of arr) {
+    if (!unique.includes(item)) {
+      unique.push(item);
+    }
+  }
+  return unique
 }
+
 
 module.exports = dedupe;
