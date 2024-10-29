03f8865ad3a7e276b6526f48487edb6dfbc86a3c
eediallo <elhhabdouldiallo@gmail.com>
2024-10-29 23:09:46 +0000

    implement --> create isFloat function is check if a number is float/decimal value

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,19 +1,22 @@
 const isEmpty = require("./isEmpty.js");
 
+function isFloat(n) {
+  return !isNaN(n) && n % 2 !== 0;
+}
+
 function sum(elements) {
   let sum = 0;
   if (isEmpty(elements)) {
     return 0;
   }
   for (let num of elements) {
-    if (!isNaN(num) && num % 2 !== 0) {
+    if (isFloat(num)) {
       sum += parseFloat(num.toFixed(2));
     } else if (!isNaN(num)) {
       sum += num;
     }
   }
   sum = parseFloat(sum.toFixed(2));
-
   return sum;
 }
 
