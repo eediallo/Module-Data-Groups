a3583f199109d611a326f27d1f15e19c00310539
eediallo <elhhabdouldiallo@gmail.com>
2024-10-29 23:53:51 +0000

    refactor --> resets initial test cases as I found them

--- Sprint-1/refactor/includes.test.js
+++ Sprint-1/refactor/includes.test.js
@@ -3,7 +3,7 @@
 const includes = require("./includes.js");
 
 test("returns true when target is in array", () => {
-  const currentOutput = includes(["a", "b", "c", "d"], ["c"]);
+  const currentOutput = includes(["a", "b", "c", "d"], "c");
   const targetOutput = true;
 
   expect(currentOutput).toEqual(targetOutput);
@@ -16,23 +16,23 @@ test("returns false when target not in array", () => {
   expect(currentOutput).toEqual(targetOutput);
 });
 
-// test("returns true when the target is in array multiple times", () => {
-//   const currentOutput = includes([1, 2, 2, 3], 2);
-//   const targetOutput = true;
+test("returns true when the target is in array multiple times", () => {
+  const currentOutput = includes([1, 2, 2, 3], 2);
+  const targetOutput = true;
 
-//   expect(currentOutput).toEqual(targetOutput);
-// });
+  expect(currentOutput).toEqual(targetOutput);
+});
 
-// test("returns false for empty array", () => {
-//   const currentOutput = includes([]);
-//   const targetOutput = false;
+test("returns false for empty array", () => {
+  const currentOutput = includes([]);
+  const targetOutput = false;
 
-//   expect(currentOutput).toEqual(targetOutput);
-// });
+  expect(currentOutput).toEqual(targetOutput);
+});
 
-// test("searches for null", () => {
-//   const currentOutput = includes(["b", "z", null, "a"], null);
-//   const targetOutput = true;
+test("searches for null", () => {
+  const currentOutput = includes(["b", "z", null, "a"], null);
+  const targetOutput = true;
 
-//   expect(currentOutput).toEqual(targetOutput);
-// });
+  expect(currentOutput).toEqual(targetOutput);
+});
