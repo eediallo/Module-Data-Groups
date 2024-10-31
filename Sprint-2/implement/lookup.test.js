4a1676329e518556821fa3b4611acaf5b9dc1b3e
eediallo <elhhabdouldiallo@gmail.com>
2024-10-31 20:33:32 +0000

    implement --> removes instruction comments

--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,37 +1,5 @@
 const createLookup = require("./lookup.js");
 
-/*
-
-Create a lookup object of key value pairs from an array of code pairs
-
-Acceptance Criteria:
-
-Given
- - An array of arrays representing country code and currency code pairs
-   e.g. [['US', 'USD'], ['CA', 'CAD']]
-
-When
- - createLookup function is called with the country-currency array as an argument
-
-Then
- - It should return an object where:
- - The keys are the country codes
- - The values are the corresponding currency codes
-
-Example
-Given: [['US', 'USD'], ['CA', 'CAD']]
-
-When
-createLookup(countryCurrencyPairs) is called
-
-Then
-It should return:
- {
-   'US': 'USD',
-   'CA': 'CAD'
- }
-*/
-
 describe("creates a country currency code lookup for multiple codes", () => {
   test("I will return an object with keys as country codes and values as corresponding currency codes when array representing country code and currency codes pairs is passed", () => {
     const countryCurrencyPairs = [
