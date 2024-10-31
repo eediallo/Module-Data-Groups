2abfcd4dc56a8e9773ad3f12e462e2467cbc7ea4
eediallo <elhhabdouldiallo@gmail.com>
2024-10-31 19:47:50 +0000

    implement --> checks if param is plain object

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,4 +1,11 @@
+function isPlainObject(obj) {
+  return Object.prototype.toString.call(obj) === "[object Object]";
+}
+
 function contains(obj, property) {
+  if (!isPlainObject(obj)) {
+    return false;
+  }
   const objKeys = Object.keys(obj); // holds keys of obj
   if (objKeys.length === 0) {
     return false;
