e8a908348603e76a150a1da6a64e0d52de58f0ab
eediallo <elhhabdouldiallo@gmail.com>
2024-10-31 20:29:35 +0000

    implement --> return an object with keys as country codes and values as corresponding currency codes when array representing country code and currency codes pairs is passed

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,11 @@
-function createLookup() {
-  // implementation here
-}
+function createLookup(countryCurrencyPairs) {
+  const codeCurrencyPairs = {};
 
+  for (let currencyPair of countryCurrencyPairs) {
+    const country = currencyPair[0];
+    const currency = currencyPair[1];
+    codeCurrencyPairs[country] = currency;
+  }
+  return codeCurrencyPairs;
+}
 module.exports = createLookup;
--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,7 +1,5 @@
 const createLookup = require("./lookup.js");
 
-test.todo("creates a country currency code lookup for multiple codes");
-
 /*
 
 Create a lookup object of key value pairs from an array of code pairs
@@ -33,3 +31,23 @@ It should return:
    'CA': 'CAD'
  }
 */
+
+describe("creates a country currency code lookup for multiple codes", () => {
+  test("I will return an object with keys as country codes and values as corresponding currency codes when array representing country code and currency codes pairs is passed", () => {
+    const countryCurrencyPairs = [
+      ["US", "USD"],
+      ["CA", "CAD"],
+      ["GUI", "GNF"],
+    ];
+    const currentOutput = createLookup(countryCurrencyPairs);
+    const targetOutput = { US: "USD", CA: "CAD", GUI: "GNF" };
+    expect(currentOutput).toEqual(targetOutput);
+  });
+
+  // test("return an empty object if param is an empty array", () => {
+  //   const countryCurrencyPairs = [];
+  //   const currentOutput = createLookup(countryCurrencyPairs);
+  //   const targetOutput = {};
+  //   expect(currentOutput).toEqual(targetOutput);
+  // });
+});
