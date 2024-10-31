b6fe7dce02a9f5ef20308e3796a3b20b2e0bc241
eediallo <elhhabdouldiallo@gmail.com>
2024-10-31 13:52:45 +0000

    adds test case for multiple pair in queryString

--- prep/object/queryString.js
+++ prep/object/queryString.js
@@ -1,10 +1,11 @@
 function parseQueryString(queryString) {
-  const queryParams = {};
+  const queryParams = {}; // to store the key-value pairs
 
-  // separate querySting with = separator
-  const [key, value] = queryString.split("=");
-
-  queryParams[key] = value; // mutate queryParams
+  const keyValuePairs = queryString.split("&"); // separate the queryString by &
+  for (let pair of keyValuePairs) {
+    const [key, value] = pair.split("="); //separate pair by =
+    queryParams[key] = value; // mutate queryParams
+  }
 
   return queryParams;
 }
--- prep/object/queryString.test.js
+++ prep/object/queryString.test.js
@@ -15,3 +15,13 @@ test("Given a query string with one pair, return an object with the key-values p
   };
   expect(currentOutput).toEqual(targetOutput);
 });
+
+test("Given a query string with multiple pairs, return an object with the key-values pair", () => {
+  const queryString = "color=blue&sort=newest";
+  const currentOutput = parseQueryString(queryString);
+  const targetOutput = {
+    color: "blue",
+    sort: "newest",
+  };
+  expect(currentOutput).toEqual(targetOutput);
+});
