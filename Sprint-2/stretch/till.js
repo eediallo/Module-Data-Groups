e20f07374bfd4566988385699e982e70050ff44b
eediallo <elhhabdouldiallo@gmail.com>
2024-11-03 22:20:22 +0000

    stretch -->  answers question of totalTill function

--- Sprint-2/stretch/till.js
+++ Sprint-2/stretch/till.js
@@ -4,28 +4,34 @@
 // When this till object is passed to totalTill
 // Then it should return the total amount in pounds
 
-function totalTill(till) {
-  let total = 0;
+// function totalTill(till) {
+//   let total = 0;
 
-  for (const [coin, quantity] of Object.entries(till)) {
-    total += coin * quantity;
-  }
+//   for (const [coin, quantity] of Object.entries(till)) {
+//     console.log("coin ==>", coin);
+//     console.log("quantity %%%%", quantity);
+//     total += Number(coin[0] * quantity);
+//   }
 
-  return `£${total / 100}`;
-}
+//   return `£${total / 100}`;
+// }
 
-const till = {
-  "1p": 10,
-  "5p": 6,
-  "50p": 4,
-  "20p": 10,
-};
-const totalAmount = totalTill(till);
+// const till = {
+//   "1p": 10,
+//   "5p": 6,
+//   "50p": 4,
+//   "20p": 10,
+// };
+// const totalAmount = totalTill(till);
+// console.log(totalAmount);
 
 // a) What is the target output when totalTill is called with the till object
+// the target output is the total amount in string format
 
 // b) Why do we need to use Object.entries inside the for...of loop in this function?
+// because Object.entries(till) returns an array with key-value pairs.
 
 // c) What does coin * quantity evaluate to inside the for...of loop?
+// it evaluates to NaN
 
 // d) Write a test for this function to check it works and then fix the implementation of totalTill
