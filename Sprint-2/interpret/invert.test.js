d99886825070789ec9fb9e73d13fedfa955a6b77
eediallo <elhhabdouldiallo@gmail.com>
2024-11-03 13:02:26 +0000

    interpret -->  returns swapped key-values when param is an object of one key-value pair

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -7,4 +7,11 @@ describe("invert key value pairs", () => {
     const targetOutput = {};
     expect(currentOutput).toEqual(targetOutput);
   });
+
+  test("It will return an object with the keys and values swapped when param object contains only one key-value pair", () => {
+    const obj = { a: 1 };
+    const currentOutput = invert(obj);
+    const targetOutput = { 1: "a" };
+    expect(currentOutput).toEqual(targetOutput);
+  });
 });
