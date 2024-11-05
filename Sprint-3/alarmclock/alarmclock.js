8f58ae7326da28c4e808fd170bd79921fd9d0768
eediallo <elhhabdouldiallo@gmail.com>
2024-11-05 22:12:32 +0000

    calculates time in minutes and remaining seconds

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,5 +1,9 @@
+const time = document.querySelector("#alarmSet").value;
+const totalTimeInSeconds = time;
+const totalMinutes = Math.floor(totalTimeInSeconds / 60);
+const remainingSeconds = totalTimeInSeconds % 60;
+
 function setAlarm() {
-  const time = document.querySelector("#alarmSet").value;
   const heading = document.querySelector("#timeRemaining");
   heading.innerText = "Time Remaining: 00:19";
 }
