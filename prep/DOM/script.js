558cca5734269e0d65a55d82196210c246f30183
eediallo <elhhabdouldiallo@gmail.com>
2024-11-05 20:43:40 +0000

    access textarea value in dev tools

--- prep/DOM/script.js
+++ prep/DOM/script.js
@@ -1,2 +1,8 @@
 const textArea = document.querySelector("textarea"); // acce
 console.log(textArea);
+
+const textAreaValue = textArea.value;
+
+textArea.addEventListener("keyup", () => {
+  console.log(textAreaValue);
+});
