08797dba8acb8440032748d8101a504d65657b64
eediallo <elhhabdouldiallo@gmail.com>
2024-11-07 15:44:14 +0000

    slideshow --> writes steps

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,16 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
 
-
 // Write your code here
+
+// steps
+// 1. query img, forward button and backward button
+// 2. displays next img when forward button is clicked
+// 3. display previous image when backward button is clicked
+
+function nexImage() {
+
+}
