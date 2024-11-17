155ff869681f73b159ecd0f6468b10671986105a
eediallo <elhhabdouldiallo@gmail.com>
2024-11-17 10:43:33 +0000

    refactors addNewTodo

--- Sprint-3/todo-list/script.js
+++ Sprint-3/todo-list/script.js
@@ -13,10 +13,9 @@ function populateTodoList(todos) {
   // 2. create an icon
 
   list.innerHTML = "";
-  todos.forEach((obj, index) => {
+  todos.forEach((obj, _) => {
     const li = document.createElement("li");
     li.textContent = obj.task;
-
     list.appendChild(li);
 
     const tickIcon = document.createElement("i");
@@ -30,6 +29,7 @@ function populateTodoList(todos) {
 
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
+
 let todos = [
   { task: "Wash the dishes", completed: false },
   { task: "Do the shopping", completed: false },
@@ -41,21 +41,14 @@ populateTodoList(todos);
 function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
-  console.log(event);
-  // Write your code here... and remember to reset the input field to be blank after creating a todo!
-  let inputField = document.querySelector("#todoInput");
-  let inputFieldValue = inputField.value;
-  console.log(inputFieldValue);
+  const inputField = event.target.querySelector("#todoInput");
+  const inputFieldValue = inputField.value;
   const newTodo = { task: inputFieldValue, completed: false };
   todos.push(newTodo);
-  inputField.value = "";
+  populateTodoList(todos);
 }
-const addBtn = document.querySelector("#btn");
-addBtn.addEventListener("click", (event) => {
-  addNewTodo(event);
-});
 
-// Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
+// Advanced challenge: Write a function that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
   // Write your code here...
 }
